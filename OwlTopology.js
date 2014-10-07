e9b2cd73c84b3cb4f2d3064ba09e35ba11fce88b
Tobiáš Potoček <tobiaspotocek@gmail.com>
2014-10-07 23:34:59 +0200

    checking if class exists before it can be added to the ontology

--- OwlTopology.js
+++ OwlTopology.js
@@ -1,6 +1,11 @@
 var fs = require('fs');
+var _ = require('lodash');
 var libxmljs = require('libxmljs');
 var Element = libxmljs.Element;
+
+// Namespace for Xpath searching
+var OWL_NAMESPACE = {'ns': 'http://www.w3.org/2002/07/owl#'};
+var IRI = 'IRI';
 
 function OwlTopology () {
 }
@@ -12,10 +17,35 @@ function OwlTopology () {
  */
 OwlTopology.prototype.loadFromFile = function (fileName) {
     var xml = fs.readFileSync(fileName).toString();
-    this.doc = libxmljs.parseXmlString(xml);
+    this.doc = libxmljs.parseXml(xml);
     this.root = this.doc.root();
 
+    // Analyze ontology
+    this._classes = this.getClasses();
+
     return this;
+};
+
+/**
+ * Get array of classes in the ontology
+ * @returns {Array}
+ */
+OwlTopology.prototype.getClasses = function () {
+    var nodes = this.doc.find('//ns:Declaration/ns:Class', OWL_NAMESPACE);
+    var classes = [];
+    _.each(nodes, function (node) {
+        classes.push(node.attr(IRI).value().replace('#', ''));
+    });
+    return classes;
+};
+
+/**
+ * Return if topology contains given class.
+ * @param className
+ * @returns {boolean}
+ */
+OwlTopology.prototype.hasClass = function (className) {
+    return _.contains(this._classes, className);
 };
 
 /**
@@ -25,6 +55,11 @@ OwlTopology.prototype.loadFromFile = function (fileName) {
  * @returns {OwlTopology}
  */
 OwlTopology.prototype.addNamedIndividual = function (className, name) {
+
+    if (!this.hasClass(className)) {
+        throw new Error('There is no class ' + className + ' in this ontology');
+    }
+
     // Add declaration
     this.root.node('Declaration')
         .node('NamedIndividual').attr({IRI: '#' + name});
