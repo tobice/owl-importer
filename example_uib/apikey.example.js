26a0a24ac1969c7e26c37b43e66fe6e7aa3b9505
Tobiáš Potoček <tobiaspotocek@gmail.com>
2014-10-08 23:31:23 +0200

    importing courses into ontology

---
+++ example_uib/apikey.example.js
@@ -0,0 +1,3 @@
+// Here goes your UiB API key that can be obtained at https://dataapi.app.uib.no/?lang=en
+// Rename this file to apikey.js
+module.exports = '1234567';
--- example_uib/import.js
+++ example_uib/import.js
@@ -1,22 +1,61 @@
 var request = require('superagent');
+var _ = require('lodash');
+var Promise = require('bluebird');
 var OwlTopology = require('./../OwlTopology');
 var UIB_API_KEY = require('./apikey.js');
+
+// Setup nice debug output
+process.env.DEBUG = process.env.DEBUG || 'error';
+var info = require('debug')('info');
+var error = require('debug')('error');
+error.log = console.error.bind(console);
 
 // Promisify superagent
 require('superagent-bluebird-promise');
 
-function makeApiUrl(action) {
-    return 'https://fs.data.uib.no/' + UIB_API_KEY + '/json/' + action;
-}
-
-request(makeApiUrl('basisinfo/emne/INFO116/2014H')).promise()
-    .then(function (response) {
-        console.log(response.body);
-    });
-/*
+var courses = 'EXPHIL-SVSEM EXPHIL-SVEKS INFO100 SV100 AORG100 ECON100 GEO100 GLOB101 MEVI100 SAMPOL100 SOS100 SANT100 POLØK100 INFO132 INFO102 INFO103 INFO110 INFO115 INFO116 INFO125 INFO216 INFO233 INFO262 INFO232 INFO282 INFO207 INFO212 INF207'.split(' ');
 var owl = new OwlTopology().loadFromFile(process.argv[2] || 'UibOntology.owl');
+info('Ontology successfully loaded');
 
-owl.addNamedIndividual('Event', 'EveningParty');
-owl.addDataPropertyAssertion('EveningParty', 'hasCredits', OwlTopology.TYPE_INT, 10);
-owl.saveToFile('UibOntologyPopulated.owl');
-    */
+new Promise(function (resolve, reject) { resolve() })
+
+    // Fetch all courses
+    .then(function() {
+        return Promise.all(_.map(courses, function (code) {
+            return makeApiCall('basisinfo/emne/' + code + '/2014H')
+                .then(function (response) {
+                    var course = response.body.emne;
+                    if (!course) {
+                        throw new Error('Course not found. Api call: ' + response.request.url);
+                    }
+
+                    info('Fetched course %s', course.emnekode);
+                    return course;
+                });
+        }));
+    })
+
+    // Parse courses and add them to ontology
+    .then(function (courses) {
+        _.each(courses, function (course) {
+            owl.addNamedIndividual('Course', course.emnekode);
+            owl.addDataPropertyAssertion(course.emnekode, 'hasTitle', OwlTopology.TYPE_STRING, course.emnenavn_engelsk);
+            owl.addDataPropertyAssertion(course.emnekode, 'hasCredits', OwlTopology.TYPE_INT, course.studiepoeng);
+            info('Course %s added to ontology', course.emnekode);
+        });
+    })
+
+    // Save ontology
+    .then(function () {
+        owl.saveToFile('UibOntologyPopulated.owl');
+        info('Ontology saved to file');
+    })
+
+    .catch(function (err) {
+        error(err);
+    });
+
+function makeApiCall(action) {
+    var url = 'https://fs.data.uib.no/' + UIB_API_KEY + '/json/' + action;
+    return request(url).promise();
+}
